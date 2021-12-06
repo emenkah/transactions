4413670daee6866a826b0b812e0dc44dc8c9b2fa
Elliot Menkah <elliotsmenkah@gmail.com>
2021-12-06 23:24:09 +0000

    Created Customer model

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Customers(models.Model):
+    name = models.CharField('Fullname', max_length=256)
+    address = models.CharField('Fullname', max_length=256)
+    checked = models.BooleanField('Checked', blank=True, null=True)
+    description = models.CharField('Fullname', max_length=256)
+    interest = models.CharField('Interest', max_length=256)
+    date_of_birth = models.DateTimeField("Date of Birth")
+    email = models.EmailField('Email address.', max_length=255, blank=True, null=True)
+    account	= models.IntegerField( 'Account No', blank=True, null=True)
+    credit_card = models.JSONField()
+
+    def __str__(self):
+        return '{}-{}-{}-{}'.format(self.name, self.email, self.account, self.date_of_birth)
+
+
+
+
