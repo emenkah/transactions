ed75678e4f2382df3319985a7ac7c497002651cf
Elliot Menkah <elliotsmenkah@gmail.com>
2021-12-09 09:52:16 +0000

    Fixed ip and port on dockerfile

--- customers/views.py
+++ customers/views.py
@@ -9,20 +9,23 @@ from .utility import date_converter, age_computer
 
 # Create your views here.
 
-def get_data(df, ind):
-    new_entry = Customers.objects.create(
-                name=df.name[ind],
-                address=df.address[ind],
-                checked=df.checked[ind],
-                description=df.description[ind],
-                interest=df.interest[ind],
-                date_of_birth=df.date_of_birth[ind],
-                email=df.email[ind],
-                account=df.account[ind],
-                credit_card=df.credit_card[ind]
+# def get_data(df, ind):
+#     new_entry = Customers.objects.create(
+#                 name=df.name[ind],
+#                 address=df.address[ind],
+#                 checked=df.checked[ind],
+#                 description=df.description[ind],
+#                 interest=df.interest[ind],
+#                 date_of_birth=df.date_of_birth[ind],
+#                 email=df.email[ind],
+#                 account=df.account[ind],
+#                 credit_card=df.credit_card[ind]
                 
-                )
-    return new_entry.save()
+#                 )
+#     new_entry.save()
+#     # print("in Get data")
+#     # print("Map entries", new_entry)
+#     # return 
 
 class ReadDataView(APIView):
     
@@ -45,6 +48,7 @@ class ReadDataView(APIView):
         '''
         df['date_of_birth'] = df['date_of_birth'].transform(date_converter)
 
+        print(df)
         last_two_records = Customers.objects.filter().order_by('id')[:2]
         print(list(last_two_records))
         
@@ -72,7 +76,7 @@ class ReadDataView(APIView):
                 )
 
         
-        #map(get_data, df, [0,1])
+
 
      
         return Response(data = {'status': 'Done'})
